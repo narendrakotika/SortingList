782590669e2d001946f9a28c18f56cd2b266a2d3
Narendra <narendrakotika3576@gmail.com>
2024-02-16 15:24:05 +0530

    added item

--- Program.cs
+++ Program.cs
@@ -13,10 +13,11 @@ public class Program
 
         // Add sample products
         productManager.AddProduct("Soap", 10.99, "Santoor", new DateTime(2022, 1, 1), new DateTime(2023, 1, 1));
-        productManager.AddProduct("Powder", 15.99, "Ponds", new DateTime(2022, 3, 1), new DateTime(2023, 3, 1));
+        productManager.AddProduct("Powder", 15.99, "Ponds", new DateTime(2023, 3, 1), new DateTime(2023, 3, 1));
         productManager.AddProduct("Face cream", 12.49, "fairandlovely", new DateTime(2022, 2, 1), new DateTime(2023, 2, 1));
         productManager.AddProduct("Face wash", 8.99, "Neem", new DateTime(2022, 5, 1), new DateTime(2023, 5, 1));
-        productManager.AddProduct("lipstick", 18.75, "Mac", new DateTime(2022, 4, 1), new DateTime(2023, 4, 1));
+        productManager.AddProduct("lipstick", 18.75, "Mac", new DateTime(2022, 4, 1), new DateTime(2025, 4, 1));
+        productManager.AddProduct("perfume", 18.75, "Denim", new DateTime(2023, 4, 1), new DateTime(2026, 4, 1));
 
         // Display sorted product details
         productManager.DisplayProducts();
--- obj/Debug/net6.0/ProductsAppAssignment4.AssemblyInfo.cs
+++ obj/Debug/net6.0/ProductsAppAssignment4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductsAppAssignment4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a2b114ee4357e34a583d06b348808264fb9f06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad30eb965dd8dcdd8b2ed3c1092abab68b62c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductsAppAssignment4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductsAppAssignment4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
